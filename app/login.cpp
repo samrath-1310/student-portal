48df4c6642bec303e198138cbaa5b7f8d47952e8
Samrath Singh <samrath1310@gmail.com>
2026-08-16 01:36:48 +0530

    Add login input validation

--- app/login.cpp
+++ app/login.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 bool validateLogin(const string& username, const string& password) {
-    return username == "student" && password == "portal123";
+    if (username.empty() || password.empty()) {
+        return false;
+    }
+    if (username != "student") {
+        return false;
+    }
+    return password == "portal123";
 }
 
 int main() {
