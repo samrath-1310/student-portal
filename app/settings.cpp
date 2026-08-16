250a22de9cb85456c476ce732bd60be0c1252af8
Samrath Singh <samrath1310@gmail.com>
2026-08-16 13:41:36 +0530

    Add portal setting toggle functions

--- app/settings.cpp
+++ app/settings.cpp
@@ -3,15 +3,28 @@
 using namespace std;
 
 void displaySettings(bool notificationsEnabled, bool darkModeEnabled) {
-    cout << "\n----Portal Settings----"<<endl;
+    cout << "----Portal Settings----"<<endl;
     cout << "Notifications: " << (notificationsEnabled ? "Enabled" : "Disabled") << endl;
     cout << "Dark Mode: " << (darkModeEnabled ? "Enabled" : "Disabled") << endl;
+}
+void toggleNotifications(bool& enabled) {
+    enabled = !enabled;
+}
+void toggleDarkMode(bool& enabled) {
+    enabled = !enabled;
 }
 
 int main() {
     bool notificationsEnabled = true;
     bool darkModeEnabled = false;
 
+    cout << "\nCurrent Settings:"<<endl;
+    displaySettings(notificationsEnabled, darkModeEnabled);
+
+    toggleNotifications(notificationsEnabled);
+    toggleDarkMode(darkModeEnabled);
+
+    cout << "Settings after changes:"<<endl;
     displaySettings(notificationsEnabled, darkModeEnabled);
 
     return 0;
